19c5e957221d487b65f7714066b0fe008ac27dc1
ramudu <nukathotiramudu@gmail.com>
2024-12-12 20:00:09 +0530

    updated code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
 import Home from './components/Home'
+import NotFound from './components/NotFound'
 
 const App = () => (
   <BrowserRouter>
     <Routes>
       <Route exact path="/" element={<Home />} />
+      <Route path='*' element={<NotFound/>} />
     </Routes>
   </BrowserRouter>
 )
